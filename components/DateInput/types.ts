a6e1ed9b9095accaf11321072ff6e534749b0da7
Mark Glattback <mark.glattback@gmail.com>
2020-12-03 23:53:16 +0000

    improved literal types

--- components/DateInput/types.ts
+++ components/DateInput/types.ts
@@ -1,20 +1,29 @@
 import { FieldProps } from "formik";
 import { ChangeEvent, InputHTMLAttributes, RefObject, SyntheticEvent } from "react";
 
+export type DayLiteral = 'D';
+export type MonthLiteral = 'M';
+export type YearLiteral = 'Y';
+export type FormattedDay = `${DayLiteral}${DayLiteral}`;
+export type FormattedMonth = `${MonthLiteral}${MonthLiteral}`;
+export type FormattedYear = `${YearLiteral}${YearLiteral}${YearLiteral}${YearLiteral}`;
+
 export interface DateInputProps extends FieldProps {
   label: string;
-  format: 'DD/MM/YYYY' | 'MM/DD/YYYY' | 'YYYY/MM/DD';
+  format: `${FormattedDay}/${FormattedMonth}/${FormattedYear}` 
+  | `${FormattedMonth}/${FormattedDay}/${FormattedYear}` 
+  | `${FormattedYear}/${FormattedMonth}/${FormattedDay}`;
 }
 
 type ParentInputName = string;
-export type DateSectionName = 'D' | 'M' | 'Y';
+export type DateSectionName = DayLiteral | MonthLiteral | YearLiteral;
 export type ComputedSectionName = `${ParentInputName}-${DateSectionName}`;
 
 export interface DateSectionProps {
   name: ComputedSectionName;
   maxLength: 2 | 4;
   onBlur: OnBlurHandler
-  onChange: OnChangeHandler;
+  onChange: ParentChangeHandler;
   onFocus: OnFocusHandler;
   section: DateSectionName;
   inputRef: RefObject<HTMLInputElement>;
@@ -40,10 +49,7 @@ export type NewDateValue = {
   [K in DateSectionName]: string;
 }
 
-export type OnChangeHandler = (section: DateSectionName, value: string, position: {
-  currentType: DateSectionName;
-  next: boolean;
-}) => void;
+export type ParentChangeHandler = (next: boolean) => void;
 
 export type OnFocusHandler = (e: SyntheticEvent<HTMLInputElement>) => void;
 export type OnBlurHandler = (e: SyntheticEvent<HTMLInputElement>) => void;
